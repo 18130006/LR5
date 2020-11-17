b6eac7ec2cc4435e665c5d9b4eb7ef7db278365b
18130006 <akhmetova.aisylu@mail.ru>
2020-11-17 21:57:03 +0500

    Lab5

---
+++ engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0474294_user';
+$password = '1234qwerty';
+$database = 'f0474294_db';
+?>
---
+++ engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+ <?require_once 'head.php';?>
+ <title>
+Ахметова Айсылу ЛР№5 ПИ-319
+</title>
+</head>
+</html>
